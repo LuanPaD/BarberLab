0d3b75a8aab6933b9cdba8177f803e4c203f9bcd
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-06-26 22:04:15 -0300

    feat: garantir que o item de reserva ocupe toda a largura da página inicial

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -44,7 +44,6 @@ interface BookingItemProps {
   }>
 }
 
-// TODO: receber agendamento como prop
 const BookingItem = ({ booking }: BookingItemProps) => {
   const [isSheetOpen, setIsSheetOpen] = useState(false)
   const {
@@ -66,7 +65,7 @@ const BookingItem = ({ booking }: BookingItemProps) => {
   }
   return (
     <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
-      <SheetTrigger className="w-full">
+      <SheetTrigger className="w-full min-w-[90%]">
         <Card className="min-w-[90%]">
           <CardContent className="flex justify-between p-0">
             {/* ESQUERDA */}
