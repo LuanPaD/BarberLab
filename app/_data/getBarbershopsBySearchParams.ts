9e05e1f1b470ed6766e035b08d37d48e09aa2081
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-06-30 14:57:36 -0300

    refactor: atualizando a action getBarbershopsBySearchParams para receber o parametro tag

--- app/_data/getBarbershopsBySearchParams.ts
+++ app/_data/getBarbershopsBySearchParams.ts
@@ -5,9 +5,22 @@ import { db } from "../_lib/prisma"
 export const getBarbershopsBySearchParams = async (searchParams: {
   title?: string
   service?: string
+  tag?: "recomendados" | "popular"
 }) => {
   if (!searchParams) {
     return []
+  }
+
+  if (searchParams.tag === "recomendados") {
+    return db.barbershop.findMany({})
+  }
+
+  if (searchParams.tag === "popular") {
+    return db.barbershop.findMany({
+      orderBy: {
+        name: "desc",
+      },
+    })
   }
 
   return db.barbershop.findMany({
